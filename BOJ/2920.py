0a624e36fe439fb3e6670242a5938752d680b0b2
orirori512 <56246060+orirori512@users.noreply.github.com>
2022-04-02 10:14:49 +0900

    2920.py

---
+++ BOJ/2920.py
@@ -0,0 +1,20 @@
+input_sound = list(map(int, input().split()))
+c_major = [1, 2, 3, 4, 5, 6, 7, 8]
+cnt = 0;
+bcnt =0;
+
+for i in range(len(c_major)):
+    if input_sound[i] == c_major[i]:
+        cnt += 1
+
+if cnt == 0:
+    for i in range(len(c_major)):
+        if input_sound[len(c_major)-i-1] == c_major[i]:
+            bcnt+=1
+
+if cnt == 8:
+    print('ascending')
+elif bcnt == 8:
+    print('descending')
+else: print('mixed')
+
