9ac3169cf515a036527d2cf672031f1eeb6020b3
JangHoonSeok <orirori512@naver.com>
2022-07-05 17:37:45 +0900

    ex 3-2

--- this-is-codingtest/Greedy/ex3-2.py
+++ this-is-codingtest/Greedy/ex3-2.py
@@ -1,1 +1,15 @@
-#큰 수의 법칙
+# 큰 수의 법칙
+import sys
+
+N, M, K = map(int, sys.stdin.readline().split())
+
+number_list = list(map(int, sys.stdin.readline().split()))
+
+number_list.sort(reverse=True)
+first_max_num = number_list[0]
+second_max_num = number_list[1]
+one_cycle = first_max_num * K + second_max_num
+
+result = (M // (K+1)) * one_cycle + (M % (K+1)) * first_max_num
+
+print(result)
