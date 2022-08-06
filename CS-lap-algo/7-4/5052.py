991a0ef6b7cb9dc285a58d9c0f30ca1527475c4a
JangHoonSeok <orirori512@naver.com>
2022-08-06 20:49:05 +0900

    5052

--- CS-lap-algo/7-4/5052.py
+++ CS-lap-algo/7-4/5052.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 
 def yes_or_no(phone_num):
     for item in phone_num:
-        for k in range(phone_num.index(item) + 1, len(phone_num)): # item 뺀 다음 index부터 확인.
+        for k in range(phone_num.index(item) + 1, len(phone_num)): # item 뺀 다음 다음 index부터 확인.
             if len(item) < len(phone_num[k]): # 길이가 같으면 전화번호가 같을 순 없으니까 길이가 긴놈들만 탐색.
                 item2 = phone_num[k][0:len(item)] # 뺀 놈의 길이만큼 앞에서 부터 잘라준다.
                 if item == item2: # 만약 같으면 NO 출력!
